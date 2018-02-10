a1a74b349e9193afc8301786ee9e478d194b6fa9
Javier Martinez <javier.martinez@unet.edu.ve>
2018-02-10 19:16:42 -0400

    Agregando estilos mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,48 +4,17 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                font-family: 'Helvetica', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                color:black;
-            }
-
-            .content {
-                text-align: center;
-            }
-            table{
-                border-spacing: 0px;
-            }
-            table, th, td {
-                color: black;
-            }
-            th,td{
-                border: 1px solid black;
-                border-collapse: collapse;
-            }
-            .duplicate {
-                background-color: yellow;
-            }
-            .error{
-                background-color: red;
-            }
-        </style>
+        <link rel="stylesheet"  href="{{mix('css/app.css')}}" >
     </head>
     <body>
+        <div id="app"></div>
         <div>
             <div class="content">
                 @yield('content')
             </div>
         </div>
+        <script  src="{{ mix('js/app.js') }}" />
     </body>
 </html>
