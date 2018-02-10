404c75572bfe499b0e8807ce4c25f99c6c5030d9
Javier Martinez <javier.martinez@unet.edu.ve>
2018-02-10 14:09:11 -0400

    agregando redireccion a resultados

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -19,6 +19,6 @@ class StorageController extends Controller
        $nombre = explode('.',$nombre);
         
        \Storage::disk('local')->put('data.' . end($nombre),  \File::get($file));
-       return 'archivo guardado';
+       return redirect('results');
     }
 }
