9760e67fc018fd36c6bd19ec2284dcce43a8d0c2
Javier Martinez <javier.martinez@unet.edu.ve>
2018-02-11 18:06:30 -0400

    dd 83

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -80,7 +80,7 @@ class StorageController extends Controller
 
                 try{
                     $rowErrors = $this->getErrors($emp);
-                    //dd($emp);
+                    dd($emp);
                     if(
                         (($emp->clave_del_ife === null)  && ($emp->clave_de_elector !== null))
                         ||
