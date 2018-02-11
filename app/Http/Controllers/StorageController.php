6c2a071b14172d46d2f59ff6ac69bf22875c7b89
Javier Martinez <javier.martinez@unet.edu.ve>
2018-02-11 19:18:13 -0400

    VUELTA

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -99,7 +99,7 @@ class StorageController extends Controller
                             'birthdate' => $emp->fecha_de_nacimiento,
                             'phone' => $emp->telefono,
                             'gender' => $emp->sexo,
-                            'rfc' => $emp->rfc,
+                            'rfc' => ltrim($emp->rfc),
                             'curp' => $emp->curp,
                             'ife_key' => $emp->clave_del_ife,
                             'elector_key' => $emp->clave_de_elector,
